6a34ee6bd1d57bc38a6f9ad236f7e55c2c9e2f39
Stanley Huang <huang.stanley050@gmail.com>
2019-06-25 16:04:43 +1000

    template for post done

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -5,8 +5,52 @@ import Image from "gatsby-image";
 import Layout from "../components/layout";
 import { MDXRenderer } from "gatsby-mdx";
 
-const PostTemplate = props => {
-  return <h1>hi</h1>;
+const PostTemplate = ({ data }) => {
+  const { title, date, author, image } = data.mdx.frontmatter;
+  const { body } = data.mdx.code;
+  const img = image.childImageSharp.fluid;
+  return (
+    <Layout>
+      <section className={styles.template}>
+        <Link to="/" className={styles.link}>
+          Back to all posts
+        </Link>
+        <div className={styles.info}>
+          <h1>{title}</h1>
+          <h4>
+            <span>by {author}</span>/<span>{date}</span>
+          </h4>
+        </div>
+        <Image fluid={img} />
+        <div className={styles.content}>
+          <MDXRenderer>{body}</MDXRenderer>
+        </div>
+      </section>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query getPost($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      code {
+        body
+      }
+      frontmatter {
+        slug
+        date(formatString: "MMMM Do YYYY")
+        author
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default PostTemplate;
